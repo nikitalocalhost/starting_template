2aee1af75a467d9a2f365cb21919f86100207349
Nikita Localhost <nikita.localhost@gmail.com>
2021-05-24 14:22:56 +0300

    Fix

--- template.sh
+++ template.sh
@@ -12,8 +12,7 @@ for file in $(find . -name "*starting_template*"); do
     mv $file `echo $file | sed s/starting_template/$sk/`;
 done
 
-find . -type f -print0 | xargs -0 sed -i `s/starting_template/$sk/g`
-
-find . -type f -print0 | xargs -0 sed -i `s/StartingTemplate/$cc/g`
+find . -type f -exec sed -i `s/starting_template/$sk/g` {} +
+find . -type f -exec sed -i `s/StartingTemplate/$cc/g` {} +
 
 rm -rf ./template.sh
